d87804e7d38a949e941fdc726ad28040c04fe685
AbdurahmanSalahEdien <100970637+AbdurahmanSalahEdien@users.noreply.github.com>
2022-04-11 05:37:58 +0200

    fiveeeeewwqqccssdddxxffcmmmkklllrrrttyyyt

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,1 +1,1 @@
-eb setenv DB_PORT=5432 EB_APP=udagram-api EB_ENV=udagram-api-dev JWT_SECRET=hello-from-other-side POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD && eb deploy --label app-220411_045323928176 udagram-api-dev
+eb setenv DB_PORT=5432 EB_APP=udagram-api EB_ENV=udagram-api-dev JWT_SECRET=hello-from-other-side POSTGRES_DB=udagram POSTGRES_HOST=localhost POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=kali && eb deploy --label app-220411_045323928176 udagram-api-dev
